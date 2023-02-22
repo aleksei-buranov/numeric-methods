1db990cccce27b1b6de5caf6768c13aebf2ca516
aleksei-buranov <steelldub@gmail.com>
2023-02-23 01:20:37 +0500

    Добавлена часть представления и ее модель для локализации корней

---
+++ ViewModels/LocalizerViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using NumericMethods.Models;
+using NumericMethods.Service;
+using System.Collections.ObjectModel;
+
+namespace NumericMethods.ViewModels
+{
+    internal class LocalizerViewModel : BaseViewModel
+    {
+        public LocalizerViewModel()
+        {
+            RootsCollection = new ObservableCollection<EquationRoots>();
+            Equation = new Equation();
+        }
+
+        public ObservableCollection<EquationRoots> RootsCollection { get; }
+        public Equation Equation { get; }
+
+        public string VisibleEquation => Equation.StringValue;
+
+        public double XMin { get; set; } = -10;
+        public double XMax { get; set; } = 10;
+        public double Step { get; set; } = 0.1;
+
+        public RelayCommand StartRootsCalculationCommand => new RelayCommand(StartRootsCalculation);
+
+        private void StartRootsCalculation()
+        {
+            Equation.SetValues(new Equation(XMin, XMax, Step));
+            RootsCollection.Clear();
+            foreach (var root in Calculator.FindEquationRoots(Equation))
+                RootsCollection.Add(root);
+        }
+    }
+}
