1acd1ffd014cd8ec73d8540e5b902abc9a7d8df0
aleksei-buranov <steelldub@gmail.com>
2023-02-23 01:17:16 +0500

    Добавлены модели уравнения и корней уравнения

---
+++ Models/Equation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NumericMethods.Models
+{
+    public class Equation
+    {
+        public static string StringValue = "F(X) = COS(X) - 0,1X";
+        public static double CalculateRoot(double x)
+        {
+            return Math.Cos(x) - 0.1 * x;
+        }
+
+        public Equation()
+        {
+        }
+
+        public Equation(double xMin, double xMax, double step)
+        {
+            XMin = xMin;
+            XMax = xMax;
+            Step = step;
+        }
+
+        public void SetValues(Equation equation2)
+        {
+            XMin = equation2.XMin;
+            XMax = equation2.XMax;
+            Step = equation2.Step;
+        }
+
+        public double XMin { get; private set; }
+
+        public double XMax { get; private set; }
+
+        public double Step { get; private set; }
+    }
+}
---
+++ Models/EquationRoots.cs
@@ -0,0 +1,14 @@
+﻿namespace NumericMethods.Models
+{
+    public class EquationRoots
+    {
+        public EquationRoots(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double X { get; }
+        public double Y { get; }
+    }
+}
