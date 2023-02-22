b56942d6b269ade658eff511abc90f4fd485ba0d
aleksei-buranov <steelldub@gmail.com>
2023-02-22 22:48:42 +0500

    Вынос окон в отдельную папку

---
+++ Views/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace NumericMethods.Views
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
