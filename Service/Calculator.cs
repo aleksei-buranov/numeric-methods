7d397b3febad4027a3ccc40e554d723f5b60260e
aleksei-buranov <steelldub@gmail.com>
2023-02-23 01:17:38 +0500

    Добавлен сервисный класс для посчета значений уравнения

---
+++ Service/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using NumericMethods.Models;
+using System.Collections.Generic;
+
+namespace NumericMethods.Service
+{
+    internal class Calculator
+    {
+        public static List<EquationRoots> FindEquationRoots(Equation equation)
+        {
+            var roots = new List<EquationRoots>();
+
+            for (double x = equation.XMin; x < equation.XMax; x += equation.Step)
+            {
+                var y = Equation.CalculateRoot(x);
+                roots.Add(new EquationRoots(x, y));
+            }
+
+            return roots;
+        }
+    }
+}
