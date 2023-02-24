ebe4157299606cc92f12e5e2beba63f5689cbdaf
aleksei-buranov <steelldub@gmail.com>
2023-02-24 14:49:07 +0500

    Добавлен компонен подсчета линейной интерполяции

---
+++ ViewModels/LinearInterpolationViewModel.cs
@@ -0,0 +1,58 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using NumericMethods.Models;
+using NumericMethods.Service;
+using System.Collections.ObjectModel;
+
+namespace NumericMethods.ViewModels
+{
+    internal class LinearInterpolationViewModel : BaseViewModel
+    {
+        private double _desiredX;
+        private double _fx;
+
+        public LinearInterpolationViewModel()
+        {
+            NodeCollection = new ObservableCollection<EquationRoots>(new[]
+            {
+                new EquationRoots(-1, 1),
+                new EquationRoots(0, 0.7272727272727273),
+                new EquationRoots(1, 0.4545454545454546),
+                new EquationRoots(2, 0.18181818181818188),
+                new EquationRoots(7, -1.1818181818181817),
+                new EquationRoots(10, -2),
+            });
+            DesiredX = 2.5;
+        }
+
+        public ObservableCollection<EquationRoots> NodeCollection { get; }
+
+        public double DesiredX
+        {
+            get => _desiredX;
+            set
+            {
+                if (value.Equals(_desiredX)) return;
+                _desiredX = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Fx
+        {
+            get => _fx;
+            private set
+            {
+                if (value.Equals(_fx)) return;
+                _fx = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand CalculateLinearInterpolationCommand => new RelayCommand(CalculateLinearInterpolation);
+
+        private void CalculateLinearInterpolation()
+        {
+            Fx = Calculator.FindLinearInterpolationValue(NodeCollection, DesiredX);
+        }
+    }
+}
